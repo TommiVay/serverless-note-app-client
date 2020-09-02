bc0674d4dcc53f472db177942bc7ec98daf18299
Tommi Vayrynen <vayrynen.tommi1996@gmail.com>
2020-09-02 13:47:26 +0300

    added favicon, navbar, routing and connection to aws

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
+
+import { Navbar, Nav } from "react-bootstrap";
 import "./App.css";
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar collapseOnSelect expand="lg" bg="light">
+        <Navbar.Brand href="/">Scratch</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-north-1",
+    BUCKET: "notes-app-uploads-tommiv",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://8ffh5dyws9.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_pm1Ou2hzF",
+    APP_CLIENT_ID: "7jccmtgconulh6gt1roq8leg5a",
+    IDENTITY_POOL_ID: "us-east-2:d10db1c6-d229-4f78-8a0f-283e75008c5a",
+  },
+};
